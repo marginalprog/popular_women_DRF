3279f9e8c7e91f458b7eca56aa0351c831791894
Ivan <korkishko.v@bk.ru>
2024-01-23 20:00:57 +0300

    реализован функционал обработки данных в сериализаторе(добавление, изменение и чтение) и представление(отображение), отвечающее за обработку запросов.

--- women/serializers.py
+++ women/serializers.py
@@ -1,9 +1,55 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from .models import Women
 
+"""class WomenModel:
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content"""
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
 
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()  # не "cat",  а cat_id
+
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_published = validated_data.get("is_published", instance.is_published)
+        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
+        instance.save()
+        return instance
+
+
+"""
+#  Аналог работы get и post. +- такая же конструкция (код и декод) лежит в Response 
+# с использованием класса сериализатора
+def encode():
+    model = WomenModel('Angelina Jolie', 'content: Angelina Jolie')
+    model_serializer = WomenSerializer(model)
+    print(model_serializer.data, type(model_serializer.data), sep='\n')
+    json = JSONRenderer().render(model_serializer.data)
+    print(json)
+
+
+def decode():
+    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"content: Angelina Jolie"}')
+    data = JSONParser().parse(stream)
+    serializer = WomenSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.is_valid())
+    print(serializer.validated_data)
+
+"""
--- women/views.py
+++ women/views.py
@@ -14,13 +14,39 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({'posts': list(lst)})  # класс преобразует словарь в json строку
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-        return Response({'post': model_to_dict(post_new)})
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()  # вызывает create из serializer
+        return Response({'post': serializer.data})
+
+    def patch(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+        else:
+            try:
+                instance = Women.objects.get(pk=pk)
+            except:
+                return Response({'error': 'Object not found'})
+
+            serializer = WomenSerializer(data=request.data, instance=instance)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method DELETE not allowed'})
+        else:
+            try:
+                instance = Women.objects.get(pk=pk)
+                instance.delete()
+            except:
+                return Response({'error': 'Object not found'})
+
+            return Response({"post": f"delete post {str(pk)}"})
