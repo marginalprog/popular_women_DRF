78db75a11f2d2c0f72828ad3a82977c5bab2d550
Ivan <korkishko.v@bk.ru>
2024-01-28 12:57:09 +0300

    Переопределение get_queryset; класс MyCustomRoute

--- popular_women/urls.py
+++ popular_women/urls.py
@@ -19,8 +19,33 @@ from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(
+            url=r'^{prefix}$',
+            mapping={
+                'get': 'list',
+            },
+            name='{basename}-list',
+            detail=False,
+            initkwargs={'suffix': 'List'}
+        ),
+        routers.Route(
+            url=r'^{prefix}/{lookup}$',
+            mapping={
+                'get': 'retrieve'
+            },
+            name='{basename}-detail',
+            detail=True,
+            initkwargs={'suffix': 'Detail'}
+        ),
+    ]
+
+
+router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')  # имя обязательно, тк убран кверисет в классе viewset
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- women/views.py
+++ women/views.py
@@ -1,16 +1,39 @@
 from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Women
+from .models import Women, Category
 from .serializers import WomenSerializer
 
 
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all() так как здесь убран кверисет, то в url добавляем basename
     serializer_class = WomenSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        # вывод части записей
+        if not pk:
+            return Women.objects.all()[:3]
+
+        # вывод конкретной записи через url
+        return Women.objects.filter(pk=pk)
+
+    """    # вывод всех категорий
+        @action(methods=['GET'], detail=False)
+        def category(self, request):
+            cats = Category.objects.all()
+            return Response({'cats': [c.name for c in cats]})
+    """
+    # вывод конкретной категории
+    @action(methods=['GET'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 
 '''class WomenAPIList(generics.ListCreateAPIView):
